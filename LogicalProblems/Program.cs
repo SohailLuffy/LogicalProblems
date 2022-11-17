c90e2185b94bdc605a1dda10d08c147a2ffa85db
SohailLuffy <sohailislammail97@gmail.com>
2022-11-17 21:41:46 +0530

    Welcome to Logical Programming Problems

---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class FB
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Problems");
+            Console.ReadLine();
+        }
+    }
+}
